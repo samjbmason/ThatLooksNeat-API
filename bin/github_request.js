deb4dc085438bd570ab4ca3a5aef56555e2afbca
Sam Mason <sam.jbmason@gmail.com>
2016-01-08 13:12:50 +0000

    Break out repeated code into default file

---
+++ bin/github_request.js
@@ -0,0 +1,21 @@
+'use strict';
+var req = require('request');
+
+let fileEndpoint = `https://api.github.com/repos/${process.env.GH_USERNAME}/${process.env.GH_REPO}/contents/${process.env.GH_DATA_FILE}`
+
+let options = {
+  uri: fileEndpoint,
+  headers: {
+    'User-Agent': process.env.GH_USERNAME
+  },
+  body: {
+    branch: 'gh-pages'
+  },
+  json: true,
+  auth: {
+    user: process.env.GH_USERNAME,
+    pass: process.env.GH_SECRET
+  }
+}
+
+module.exports = req.defaults(options);
